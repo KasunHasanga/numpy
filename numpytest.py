5419694354096421d257e5f42fe51c3bcdd92270
[Kasun Hasanga] <[kasunhasanga96@gmail.com]>
2019-05-10 22:30:18 +0100

    neww commit

--- numpytest.py
+++ numpytest.py
@@ -2,14 +2,13 @@ from numpy import *
 
 arr1 = array([1,2,3,4,5,6])
 arr2 = array([6,5,4,3,3,5])
-arr3 = arr2-+arr1
 
-arr4=arr3.copy()
+arr3=arr1.copy()
+
+
+arr3[3]=6
 print(arr3)
-
+print(arr1)
+print(concatenate([arr1,arr3]))
 print(id(arr1))
-print(id(arr2))
 print(id(arr3))
-print(id(arr4), "arry 4")
-
-
